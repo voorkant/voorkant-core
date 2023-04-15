33b132445a9548387eb89035edf63479236545e0
Peter van Dijk <peter@7bits.nl>
2023-04-16 01:10:37 +0200

    reindent to 2 spaces

--- main.cpp
+++ main.cpp
@@ -19,142 +19,142 @@ static const uint32_t ID_SUBSCRIPTION = 1;
 class HAEntity
 {
 public:
-    HAEntity(void) {
+  HAEntity(void) {
 
-    }
-    HAEntity(json _state) {
-        state = _state;
-    }
-    ~HAEntity() {
+  }
+  HAEntity(json _state) {
+    state = _state;
+  }
+  ~HAEntity() {
 
-    }
+  }
 
-    void update(json _state) {
-        state = _state;
-    }
+  void update(json _state) {
+    state = _state;
+  }
 
-    std::string toString(void) {
-        return state.dump();
-    }
+  std::string toString(void) {
+    return state.dump();
+  }
 
-    std::string getState(void) {
-        return state["state"];
-    }
+  std::string getState(void) {
+    return state["state"];
+  }
 private:
-    json state;
+  json state;
 };
 
 class WSConn
 {
 public:
-    WSConn(std::string url) {
-        wshandle = curl_easy_init();
+  WSConn(std::string url) {
+    wshandle = curl_easy_init();
 
-        curl_easy_setopt(wshandle, CURLOPT_CONNECT_ONLY, 2L);
-        curl_easy_setopt(wshandle, CURLOPT_URL, url.c_str());
-        curl_easy_perform(wshandle);
-    }
-    ~WSConn() {
+    curl_easy_setopt(wshandle, CURLOPT_CONNECT_ONLY, 2L);
+    curl_easy_setopt(wshandle, CURLOPT_URL, url.c_str());
+    curl_easy_perform(wshandle);
+  }
+  ~WSConn() {
         // FIXME clean up here
-    }
+  }
 
-    std::string recv(void) {
-        size_t recv;
-        struct curl_ws_frame *meta;
-        struct pollfd pfd;
+  std::string recv(void) {
+    size_t recv;
+    struct curl_ws_frame *meta;
+    struct pollfd pfd;
 
-        pfd.events = POLLIN;
+    pfd.events = POLLIN;
         /* cerr<< */ curl_easy_getinfo(wshandle, CURLINFO_ACTIVESOCKET, &pfd.fd) /* <<endl */ ;
 
-        CURLcode ret;
+    CURLcode ret;
 
         // FIXME: handle frames > 64k
-        while((ret = curl_ws_recv(wshandle, buffer, sizeof(buffer), &recv, &meta)) == CURLE_AGAIN) {
+    while((ret = curl_ws_recv(wshandle, buffer, sizeof(buffer), &recv, &meta)) == CURLE_AGAIN) {
           // cerr<<"CURLE_AGAIN"<<endl;
-          poll(&pfd, 1, 1000);
-        }
+      poll(&pfd, 1, 1000);
+    }
         // cerr<<ret<<endl;
         // cerr<<buffer<<endl;
-        return std::string(buffer, recv);
+    return std::string(buffer, recv);
 
-    }
+  }
 
-    void send(std::string& msg) {
-        size_t sent;
-        curl_ws_send(wshandle, msg.c_str(), msg.length(), &sent, 0, CURLWS_TEXT);
-    }
-    
-    CURL* wshandle;
-    char buffer[64000];
+  void send(std::string& msg) {
+    size_t sent;
+    curl_ws_send(wshandle, msg.c_str(), msg.length(), &sent, 0, CURLWS_TEXT);
+  }
+
+  CURL* wshandle;
+  char buffer[64000];
 };
 
 int main(void) // int /* argc */, char* /* argv[] */*)
 {
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    auto wc = WSConn(std::string(getenv("HA_WS_URL")));
+  curl_global_init(CURL_GLOBAL_ALL);
+  auto wc = WSConn(std::string(getenv("HA_WS_URL")));
 
-    auto welcome = wc.recv();
+  auto welcome = wc.recv();
 
-    auto jwelcome = json::parse(welcome);
+  auto jwelcome = json::parse(welcome);
 
-    cerr<<"got welcome: "<<welcome<<endl; // FIXME check that it is the expected auth_required message
+  cerr<<"got welcome: "<<welcome<<endl; // FIXME check that it is the expected auth_required message
 
-    json auth;
+  json auth;
 
-    auth["type"] = "auth";
-    auth["access_token"] = getenv("HA_API_TOKEN");
+  auth["type"] = "auth";
+  auth["access_token"] = getenv("HA_API_TOKEN");
 
-    cerr<<auth.dump()<<endl;
+  cerr<<auth.dump()<<endl;
 
-    auto jauth = auth.dump();
-    cerr<<jauth<<endl;
-    wc.send(jauth);
+  auto jauth = auth.dump();
+  cerr<<jauth<<endl;
+  wc.send(jauth);
 
-    cerr<<wc.recv()<<endl; // FIXME assert auth_ok
+  cerr<<wc.recv()<<endl; // FIXME assert auth_ok
 
-    json subscribe;
+  json subscribe;
 
-    subscribe["id"] = ID_SUBSCRIPTION;
-    subscribe["type"] = "subscribe_events";
+  subscribe["id"] = ID_SUBSCRIPTION;
+  subscribe["type"] = "subscribe_events";
 
-    auto jsubscribe = subscribe.dump();
+  auto jsubscribe = subscribe.dump();
 
-    wc.send(jsubscribe);
+  wc.send(jsubscribe);
 
-    map<string, std::shared_ptr<HAEntity>> states;
+  map<string, std::shared_ptr<HAEntity>> states;
 
-    while (true) {
-        auto msg = wc.recv();
+  while (true) {
+    auto msg = wc.recv();
 
-        json j = json::parse(msg);
+    json j = json::parse(msg);
 
-        if (j["id"] != ID_SUBSCRIPTION) {
-            continue;
-        }
-
-        auto event = j["event"];
-        auto event_type = event["event_type"];
-        auto evd = event["data"];
-        auto entity_id = evd["entity_id"];
-
-        auto old_state = evd["old_state"];
-        auto new_state = evd["new_state"];
-
-        cout << "event_type=" << event_type << ", ";
-        cout << "entity_id=" << entity_id << ", ";
-        cout << "state=" << new_state["state"];
-        cout << endl;
-
-        if (event_type == "state_changed") {
-            states[entity_id] = std::make_shared<HAEntity>(new_state);
-        }
-
-        cerr<<"\033[2Jhave "<<states.size()<< " states" << endl;
-        cerr<<endl;
-        for (auto &[k,v] : states) {
-            cout<<k<<"="<<v->getState()<<endl;
-        }
+    if (j["id"] != ID_SUBSCRIPTION) {
+      continue;
     }
-    return 0;
+
+    auto event = j["event"];
+    auto event_type = event["event_type"];
+    auto evd = event["data"];
+    auto entity_id = evd["entity_id"];
+
+    auto old_state = evd["old_state"];
+    auto new_state = evd["new_state"];
+
+    cout << "event_type=" << event_type << ", ";
+    cout << "entity_id=" << entity_id << ", ";
+    cout << "state=" << new_state["state"];
+    cout << endl;
+
+    if (event_type == "state_changed") {
+      states[entity_id] = std::make_shared<HAEntity>(new_state);
+    }
+
+    cerr<<"\033[2Jhave "<<states.size()<< " states" << endl;
+    cerr<<endl;
+    for (auto &[k,v] : states) {
+      cout<<k<<"="<<v->getState()<<endl;
+    }
+  }
+  return 0;
 }
