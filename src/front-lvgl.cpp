1f90306cda692cf590aaf026e28f33b51f43a21f
Peter van Dijk <peter@7bits.nl>
2023-12-13 21:18:19 +0100

    build against lvgl

---
+++ src/front-lvgl.cpp
@@ -0,0 +1,69 @@
+#include "main.hpp"
+
+#include "argparse.hpp"
+#include "src/argparse.hpp"
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+#include <lvgl.h>
+
+using std::string;
+// using std::map;
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::flush;
+
+void uithread(WSConn & /* wc */, int argc, char* argv[])
+{
+    cerr<<"calling lv_init"<<endl;
+    lv_init();
+    cerr<<"called lv_init"<<endl;
+
+
+    argparse::ArgumentParser program("client-cli");
+
+    argparse::ArgumentParser subscribe_command("subscribe");
+    subscribe_command.add_argument("pattern")
+      .help("specific state name, or *"); // maybe .remaining() so you can subscribe multiple?
+
+    program.add_subparser(subscribe_command);
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        cerr << err.what() << endl;
+        cerr << program;
+        return;
+    }
+
+    if (program.is_subcommand_used(subscribe_command)) {
+        // FIXME: now actually use the argument
+        cerr << "should subscribe to " << subscribe_command.get<string>("pattern") << endl;
+    }
+    else {
+        cerr << "no command given" << endl;
+    }
+
+    while(true) {
+        sleep(1);
+        cerr<<"."<<flush;
+    }
+}
+
+void uithread_refresh(std::vector<std::string> whatchanged) // would be cool if this got told what changed
+{
+    return;
+    std::scoped_lock lk(entrieslock, stateslock, domainslock);
+
+    for(const auto &changed : whatchanged) {
+        cout<<"state for "<<changed<<" is "<<states[changed]->getInfo()<<endl;
+        for(const auto &attr : states[changed]->attrVector()) {
+            cout<<"  " << attr <<endl;
+        }
+    }
+}
