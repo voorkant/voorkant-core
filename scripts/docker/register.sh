ab2b0b066a3c135c7f61f31217d84929ac6e333a
Ruben d'Arco <cyclops-github@prof-x.net>
2023-12-14 19:46:35 +0000

    Add tmux.sh and modify register.sh to startup 'dev' environment.

--- scripts/docker/register.sh
+++ scripts/docker/register.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+CHECK_COUNT=0
+curl 'http://localhost:8123/'
+while [ $? -ne 0 ]
+do
+    if [ $CHECK_COUNT -gt 10 ]; then
+        echo "HA doesn't seem to be started after a while, giving up."
+        exit 1
+    fi
+
+    echo 'HA still down...'
+    sleep 1
+    CHECK_COUNT=$((CHECK_COUNT+1))
+
+    # Keep on the last line because of exit status check
+    curl 'http://localhost:8123'
+done
+
+
 # Create voorkant user
 curl 'http://localhost:8123/api/onboarding/users' -X POST  --data-raw '{"client_id":"http://localhost:8123/","name":"voorkant","username":"voorkant","password":"v00rk4nt","language":"en-GB"}' > auth_code.json
 MSG=`jq -r '.message' auth_code.json`
@@ -11,7 +29,7 @@ if [ "${MSG}" != "User step already done" ]; then
     # To complete setup, you have to set some other stuff. For whatever reason (might be because demo mode) these requests are empty in our capture.
     curl 'http://localhost:8123/api/onboarding/core_config' -X POST -H "authorization: Bearer ${TOKEN}"
     curl 'http://localhost:8123/api/onboarding/analytics' -X POST -H "authorization: Bearer ${TOKEN}"
-    curl 'http://localhost:8123/api/onboarding/integration'  -X POST -H "authorization: Bearer ${TOKEN}"
+    curl 'http://localhost:8123/api/onboarding/integration'  -X POST -H "authorization: Bearer ${TOKEN}" -d '{"client_id":"http://localhost:8123/","redirect_uri":"http://localhost:8123/?auth_callback=1"}'
 fi
 
 # rm auth_code.json
---
+++ scripts/tmux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -x
+
+PWD=`pwd`
+ROOT=`dirname ${PWD}`
+
+
+SESSIONNAME="VOORKANT"
+tmux has-session -t $SESSIONNAME
+
+if [ $? != 0 ] 
+then
+    tmux new-session -s $SESSIONNAME -n HA -d
+    tmux send-keys -t $SESSIONNAME "cd ${ROOT}/scripts/docker && docker-compose up" C-m
+    tmux new-window -t $SESSIONNAME -n REGISTER 
+    tmux send-keys -t $SESSIONNAME "cd ${ROOT}/scripts/docker && ./register.sh" C-m
+fi
+
+tmux attach -t $SESSIONNAME
