8f92aac19c86be788a9f5369238a6bf863b9d376
Peter van Dijk <peter@7bits.nl>
2023-12-26 20:53:15 +0100

    add a button!

--- src/front-lvgl.cpp
+++ src/front-lvgl.cpp
@@ -20,6 +20,20 @@ using std::cerr;
 using std::endl;
 using std::flush;
 
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Button: %d", cnt);
+    }
+}
+
 static uint32_t c=0;
 void uithread(WSConn & /* wc */, int argc, char* argv[])
 {
@@ -29,6 +43,7 @@ void uithread(WSConn & /* wc */, int argc, char* argv[])
     cerr<<"called lv_init and sdl_init"<<endl;
 
 #define MY_DISP_HOR_RES 480
+
     /*Create a display buffer*/
     static lv_disp_draw_buf_t disp_buf;
     static lv_color_t buf_1[SDL_HOR_RES * SDL_VER_RES];
@@ -43,6 +58,26 @@ void uithread(WSConn & /* wc */, int argc, char* argv[])
     disp_drv.flush_cb = sdl_display_flush;        /*Set a flush callback to draw to the display*/
     lv_disp_t * disp;
     disp = lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
+
+    lv_indev_drv_t enc_drv;
+    lv_indev_drv_init(&enc_drv);
+    enc_drv.type = LV_INDEV_TYPE_POINTER;
+    enc_drv.read_cb = sdl_mouse_read;
+    lv_indev_t * enc_indev = lv_indev_drv_register(&enc_drv);
+    // lv_indev_set_group(enc_indev, g);
+    lv_group_t * g = lv_group_create();
+    lv_group_set_default(g);
+
+// START BUTTON EXAMPLE
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
+// END BUTTON EXAMPLE
 
     argparse::ArgumentParser program("client-cli");
 
