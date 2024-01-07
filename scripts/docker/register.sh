1b6817dd098f711512d5de81b321e815fad2ebb3
Ruben d'Arco <cyclops-github@prof-x.net>
2024-01-07 20:46:51 +0000

    Update register to retrieve long token
    We compile our software and then use it in the script.
    
    Also cleaned up some output to make the script nicer.
    Finally, we output the environment variables needed.

--- scripts/docker/register.sh
+++ scripts/docker/register.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# We need to compile our code for the ha-long-token
+cd ../../
+meson setup build
+meson compile -C build
+
+
 CHECK_COUNT=0
 curl 'http://localhost:8123/'
 while [ $? -ne 0 ]
@@ -19,22 +25,22 @@ done
 
 
 # Create voorkant user
-curl 'http://localhost:8123/api/onboarding/users' -X POST  --data-raw '{"client_id":"http://localhost:8123/","name":"voorkant","username":"voorkant","password":"v00rk4nt","language":"en-GB"}' > auth_code.json
+curl 'http://localhost:8123/api/onboarding/users' -s -X POST  --data-raw '{"client_id":"http://localhost:8123/","name":"voorkant","username":"voorkant","password":"v00rk4nt","language":"en-GB"}' > auth_code.json
 MSG=`jq -r '.message' auth_code.json`
 if [ "${MSG}" != "User step already done" ]; then
     CODE=`jq -r '.auth_code' auth_code.json`
-    curl 'http://localhost:8123/auth/token' -X POST -F "grant_type=authorization_code" -F "code=${CODE}" -F "client_id=http://localhost:8123/" > token.json
+    curl 'http://localhost:8123/auth/token' -s -X POST -F "grant_type=authorization_code" -F "code=${CODE}" -F "client_id=http://localhost:8123/" > token.json
     TOKEN=`jq -r '.access_token' token.json`
-
     # To complete setup, you have to set some other stuff. For whatever reason (might be because demo mode) these requests are empty in our capture.
-    curl 'http://localhost:8123/api/onboarding/core_config' -X POST -H "authorization: Bearer ${TOKEN}"
-    curl 'http://localhost:8123/api/onboarding/analytics' -X POST -H "authorization: Bearer ${TOKEN}"
-    curl 'http://localhost:8123/api/onboarding/integration'  -X POST -H "authorization: Bearer ${TOKEN}" -d '{"client_id":"http://localhost:8123/","redirect_uri":"http://localhost:8123/?auth_callback=1"}'
+    curl 'http://localhost:8123/api/onboarding/core_config' -s -X POST -H "authorization: Bearer ${TOKEN}" > /dev/null
+    curl 'http://localhost:8123/api/onboarding/analytics' -s -X POST -H "authorization: Bearer ${TOKEN}" > /dev/null
+    curl 'http://localhost:8123/api/onboarding/integration'  -s -X POST -H "authorization: Bearer ${TOKEN}" -d '{"client_id":"http://localhost:8123/","redirect_uri":"http://localhost:8123/?auth_callback=1"}' > /dev/null
 fi
 
 rm auth_code.json
 rm token.json
 
-echo
-echo
-echo HA_WS_URL=ws://localhost:8123/api/websocket HA_API_TOKEN="${TOKEN}"
+HA_WS_URL=ws://localhost:8123/api/websocket HA_API_TOKEN="${TOKEN}" LD_LIBRARY_PATH=build/subprojects/curl-8.5.0/build/lib/.libs/ build/client-cli ha-get-token 1>longtoken.txt
+
+echo HA_WS_URL=ws://localhost:8123/api/websocket HA_API_TOKEN="`cat longtoken.txt`"
+rm longtoken.txt
