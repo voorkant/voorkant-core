8964a30ef8c3cd3893ec74c659e72f21e2697229
Ruben d'Arco <cyclops-github@prof-x.net>
2024-01-26 20:18:55 +0000

    Padding around main flexbox

--- src/front-lvgl.cpp
+++ src/front-lvgl.cpp
@@ -206,6 +206,8 @@ void uithread(HABackend& backend, int argc, char* argv[])
   lv_obj_remove_style_all(cont_row);
   lv_obj_set_size(cont_row, SDL_HOR_RES, SDL_VER_RES);
   lv_obj_set_flex_flow(cont_row, LV_FLEX_FLOW_COLUMN_WRAP);
+  lv_obj_set_style_pad_row(cont_row, 10, LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_pad_column(cont_row, 10, LV_PART_MAIN | LV_STATE_DEFAULT);
 
   std::vector<std::unique_ptr<UIEntity>> uielements;
   int i = 0;
