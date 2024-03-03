47a7cc719ae969f0904939b708b0c2b3b8c0b7a2
Ruben d'Arco <cyclops-github@prof-x.net>
2024-03-03 12:09:57 +0000

    NOLINT for curl includes

--- src/main.hpp
+++ src/main.hpp
@@ -4,9 +4,10 @@
 #include <mutex>
 
 #include <poll.h>
-
+// NOLINTBEGIN(clang-diagnostic-error])
 #include <curl/curl.h>
 #include <curl/easy.h>
+// NOLINTEND(clang-diagnostic-error])
 
 #include <nlohmann/json.hpp>
 #include "Backend.hpp"
