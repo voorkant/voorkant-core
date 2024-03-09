be68d5596080f881aea4adc56c4fd48c6244a8d2
Ruben d'Arco <cyclops-github@prof-x.net>
2024-03-09 15:59:53 +0000

    NOLINT for bugprone-macro-parentheses.
    Ok, to me this just looks like a use-case that they (clang-tidy) have not considered.
    The define here is just as the most basic text replacement.
    This use-case does not seem to exist in
    https://github.com/microsoft/clang-tools-extra/blob/master/test/clang-tidy/misc-macro-parentheses.cpp or
    https://github.com/llvm/llvm-project/blob/main/clang-tools-extra/test/clang-tidy/checkers/bugprone/macro-parentheses.cpp
    
    I've also reported this here: https://github.com/llvm/llvm-project/issues/84636

--- src/lv_drv_conf.h
+++ src/lv_drv_conf.h
@@ -108,7 +108,7 @@
 #  define SDL_DOUBLE_BUFFERED 0
 
 /*Eclipse: <SDL2/SDL.h>    Visual Studio: <SDL.h>*/
-#  define SDL_INCLUDE_PATH    <SDL2/SDL.h>
+#  define SDL_INCLUDE_PATH    <SDL2/SDL.h> //NOLINT(bugprone-macro-parentheses)
 
 /*Open two windows to test multi display support*/
 #  define SDL_DUAL_DISPLAY            0
