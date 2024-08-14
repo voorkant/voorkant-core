7db62a2fe1f6016c565b309e39b03c5bcca09117
Peter van Dijk <peter@7bits.nl>
2024-08-14 19:19:03 +0200

    switch from docker-compose to docker compose

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -15,7 +15,7 @@ then
     sudo rm -rf ${ROOT}/scripts/docker/ha-config
     cp -r ${ROOT}/scripts/docker/config ${ROOT}/scripts/docker/ha-config
     tmux new-session -s $SESSIONNAME -n HA -d
-    tmux send-keys -t $SESSIONNAME "cd ${ROOT}/scripts/docker && docker-compose up" C-m
+    tmux send-keys -t $SESSIONNAME "cd ${ROOT}/scripts/docker && docker compose up" C-m
     tmux new-window -t $SESSIONNAME -n REGISTER 
     tmux send-keys -t $SESSIONNAME "cd ${ROOT}/scripts/docker && ./register.sh" C-m
 fi
