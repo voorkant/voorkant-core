0bebec866d91da512efbe21b8c8c726c460c21f8
Peter van Dijk <peter@7bits.nl>
2024-10-20 21:46:15 +0200

    note frozen cstdint might have been fixed meanwhile

--- src/mdimap.hpp
+++ src/mdimap.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cstdint> // FIXME: this works around a bug in frozen?
+#include <cstdint> // FIXME: this works around a bug in frozen? (likely fixed in 1.2.0, we're using 1.1.1 here)
 
 #include <frozen/unordered_map.h>
 #include <frozen/string.h>
