ee557b0514964df560537389613a20a71bf86ca9
Peter van Dijk <peter@7bits.nl>
2024-10-20 21:46:02 +0200

    clang-format; leaves lack of newline at end of file, should check clang format config

--- src/mdimap.hpp
+++ src/mdimap.hpp
@@ -5,8 +5,10 @@
 #include <frozen/unordered_map.h>
 #include <frozen/string.h>
 
-namespace voorkant {
-namespace mdi{
-extern const frozen::unordered_map<frozen::string, frozen::string, 4> name2id;
+namespace voorkant
+{
+namespace mdi
+{
+  extern const frozen::unordered_map<frozen::string, frozen::string, 4> name2id;
 }
 }
