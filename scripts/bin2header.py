00b0b181f97a5352913c149987f7cd3fbafa7430
Peter van Dijk <peter@7bits.nl>
2024-12-26 22:39:23 +0100

    adjust for python3

--- scripts/bin2header.py
+++ scripts/bin2header.py
@@ -11,11 +11,11 @@ def bin2header(data, var_name='var'):
     out.append('unsigned char {var_name}[] = {{'.format(var_name=var_name))
     l = [ data[i:i+12] for i in range(0, len(data), 12) ]
     for i, x in enumerate(l):
-        line = ', '.join([ '0x{val:02x}'.format(val=ord(c)) for c in x ])
+        line = ', '.join([ '0x{val:02x}'.format(val=c) for c in x ])
         out.append('  {line}{end_comma}'.format(line=line, end_comma=',' if i<len(l)-1 else ''))
     out.append('};')
     out.append('unsigned int {var_name}_len = {data_len};'.format(var_name=var_name, data_len=len(data)))
-    return '\n'.join(out)
+    return '\n'.join(out).encode('ascii')
 
 def main():
     parser = argparse.ArgumentParser(description='Generate binary header output')
@@ -27,11 +27,11 @@ def main():
     if not args:
         return 1
 
-    with open(args.input, 'r') as f:
+    with open(args.input, 'rb') as f:
         data = f.read()
 
     out = bin2header(data, args.var)
-    with open(args.out, 'w') as f:
+    with open(args.out, 'wb') as f:
         f.write(out)
 
     return 0
